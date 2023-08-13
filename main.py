930fbd08def509a39538e9e8d7d3f15e9f45ad85
Максим <lebedevmaximdmitrievich@gmail.com>
2023-08-13 12:57:26 +0300

    Реализовал HTTP запросы с помощью библиотеки fastAPI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
+from fastapi import FastAPI
 
+app = FastAPI()
 keys = [
     'geonameid',
     'name',
@@ -28,45 +29,43 @@ keys_timezone = [
     'offset',
     'raw offset'
 ]
-data = []
-timezone = []
+
+def read_data_from_file(filename, keys):
+    lines = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            combined_dict = {}
+            for i in range(len(keys)):
+                combined_dict[keys[i]] = line.strip('\n').split('\t')[i]  # Добавляем элемент в словарь
+            lines.append(combined_dict)
+        return lines
 
 
-# Чтение и перенос данных о городах в память
-with open('RU.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        combined_dict = {}
-        for i in range(len(keys)):
-            combined_dict[keys[i]] = line.strip('\n').split('\t')[i]  # Добавляем элемент в словарь
-        data.append(combined_dict)
-
-# Чтение и перенос данных о часовых поясах
-with open('TimeZone.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        combined_dict = {}
-        for i in range(len(keys_timezone)):
-            combined_dict[keys_timezone[i]] = line.strip('\n').split('\t')[i]  # Добавляем элемент в словарь
-        timezone.append(combined_dict)
+data = read_data_from_file('RU.txt', keys)
+timezone = read_data_from_file('TimeZone.txt', keys_timezone)
 
 
 # Метод возвращает словарь по geonameid в случае нахождения, и None в случае если такого словаря нет
-def get_info(geonameid):
+@app.get('/city/{geo_name_id}')
+def get_info(geo_name_id):
     for city in data:
-        if city.get('geonameid') == geonameid:
-            return json.dumps(city, ensure_ascii=False)
+        if city.get('geonameid') == geo_name_id:
+            return city
 
 
 # Метод возвращает массив словарей с условием пользователя, массив разбивается на "страницы" и выводит все словари
 # находящиеся на этой странице
-def get_cities(page_number, cities_per_page):
+@app.get('/cities')
+def get_cities(page_number: int, cities_per_page: int):
     start_index = (page_number - 1) * cities_per_page
     end_index = start_index + cities_per_page
     cities_on_page = data[start_index:end_index]
 
-    return json.dumps(cities_on_page, ensure_ascii=False)
+    return cities_on_page
 
 
 # Метод принимает 2 города, выводит их, какой город севернее и одинаковые ли часовые пояса
+@app.get('/cities/different')
 def get_different_by_cities(first_city, second_city):
     f_founded_cities = []
     s_founded_cities = []
@@ -90,25 +89,19 @@ def get_different_by_cities(first_city, second_city):
     s_city = sorted(s_founded_cities, key=lambda city_population: int(city_population['population']), reverse=True)[0]
     # Определение северного города
     higher_latitude_city = f_city if f_city.get('latitude') > s_city.get('latitude') else s_city
-    # Проверка на одинаковые часовые пояса
+    # Проверка на одинаковые часовые пояса и вычисление разницы между ними
     is_same_timezone = f_city.get('timezone') == s_city.get('timezone')
-    difference_timezones = None
-    if not is_same_timezone:
-        difference_timezones = abs(float(get_gmt(f_city.get('timezone'))) - float(get_gmt(s_city.get('timezone'))))
+    difference_timezones = abs(float(get_gmt(f_city.get('timezone'))) - float(get_gmt(s_city.get('timezone'))))
 
-    print(f'Самый северный город: {higher_latitude_city}')
-    print(f'Одинаковая ли временная зона: {is_same_timezone}')
-    if difference_timezones is not None:
-        print(f'Разница между городами в часовых поясах: {difference_timezones}')
-    print(f'Первый город: {f_city}\nВторой город: {s_city}')
+    return {'higher_latitude_city': higher_latitude_city,
+            'is_same_timezone: ': is_same_timezone,
+            'difference_timezones: ': difference_timezones,
+            'f_city: ': f_city,
+            's_city: ': s_city,
+            }
 
 
 def get_gmt(time_zone_id):
     for zone in timezone:
         if zone.get('time zone id') == time_zone_id:
             return zone.get('GMT')
-
-
-print(get_info('12531557'))
-print(get_cities(10, 20))
-print(get_different_by_cities('Пенза', 'Барнаул'))
